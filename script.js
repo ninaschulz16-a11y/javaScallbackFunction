cb4919d99632b12d3736882c1eba669a7ea28c15
Nina E Schulz <ninaschulz_16@hotmail.com>
2025-09-10 11:57:51 +0100

    some objects learning

---
+++ script.js
@@ -0,0 +1,16 @@
+
+console.log('Time to shine baby!')
+
+const pet = {
+
+    name: "Lexi",
+    age: 13,
+    colour: "dark brown",
+    favouriteFood : "everything",
+
+};
+
+console.log(pet.name);
+console.log(pet.age);
+console.log(pet.colour);
+console.log(pet.favouriteFood);
